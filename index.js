9880327e82fbc1661fad1fb2c9d4d37550e6dccd
david dacruz <david@davids-Air.Home>
2022-08-07 10:28:20 +0100

    extract data

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ import axios from 'axios'
 
         const alchemyApi = 'https://eth-mainnet.g.alchemy.com/nft/v2/demo/getOwnersForCollection?contractAddress=0xd374410e9bb22f3771ffbd0b40a07c0cf44a04fc&withTokenBalances=true'
 
-        const r = await axios.get(alchemyApi)
+        const { data: { ownerAddresses }
+        } = await axios.get(alchemyApi)
 
         let owners = '';
 
-        for (let i = 0; i < r.data.ownerAddresses.length; i++) {
-            owners += `${r.data.ownerAddresses[i].ownerAddress}, ${r.data.ownerAddresses[i].tokenBalances[0].balance}\r\n`;
+        for (let i = 0; i < ownerAddresses.length; i++) {
+            owners += `${ownerAddresses[i].ownerAddress}, ${ownerAddresses[i].tokenBalances[0].balance}\r\n`;
         }
 
         fs.writeFileSync(`./snapshot_${Date.now()}.csv`, owners)
